5a95c57cf52eb91f06e49dbd21be50324ec758bb
varuntyagiG <varungauravtyagi@gmail.com>
2024-06-04 23:30:12 +0530

    Lottery-Game

---
+++ src/Helper.js
@@ -0,0 +1,15 @@
+function genTicket(n) {
+  let arr = new Array(n);
+  for (let i = 0; i < n; i++) {
+    arr[i] = Math.floor(Math.random() * 10);
+  }
+  return arr;
+}
+
+function sum(arr) {
+  return arr.reduce((el, val) => {
+    return el + val;
+  });
+}
+
+export { sum, genTicket };
---
+++ src/Lottery.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./Lottery.css";
+import { genTicket, sum } from "./Helper";
+
+function Lottery() {
+  let [ticket, setTicket] = useState(genTicket(3));
+  let isWinning = sum(ticket) === 15;
+
+  function gennerator() {
+    setTicket(genTicket(3));
+  }
+
+  return (
+    <div>
+      <h1>Lottery Game!</h1>
+      <div className="ticket">
+        <span>{ticket[0]}</span>
+        <span>{ticket[1]}</span>
+        <span>{ticket[2]}</span>
+        <br />
+        <br />
+        <button onClick={gennerator}>buy ticket</button>
+        <h2>{isWinning && "Congratulations ! you won the game"}</h2>
+      </div>
+    </div>
+  );
+}
+
+export { Lottery };
